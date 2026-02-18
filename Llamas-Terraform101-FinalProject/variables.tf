6ea8fca5e1d955c21282b8d46d616987975fd49f
Maxine Sofia <maxinesofia.education@gmail.com>
2026-02-18 11:23:59 +0800

    Delete Llamas-Terraform101-FinalProject directory

--- Llamas-Terraform101-FinalProject/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "lastname" {}
-variable "firstname" {}
-variable "project_name" {}
-variable "project_code" {}
-variable "region" {}
-variable "key_name" {
-  type        = string
-  description = "The name of the AWS Key Pair"
-}
